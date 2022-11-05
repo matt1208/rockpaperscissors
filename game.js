a7d11edaec4c436359bc9f25648b549f637dfccc
Matthew <matthewvandyke08@gmail.com>
2022-11-05 12:41:51 -0700

    Display Winner

--- game.js
+++ game.js
@@ -1,7 +1,13 @@
 var name = window.prompt("Please enter your name?");
 document.write(name + ", Welcome to Rock Paper Scissors!");
-var option = ["rock", "paper", "scissors"];
+var option = ["Rock", "Paper", "Scissors"];
 var player = "";
+var computerResult = "";
+var rock = "Rock";
+var scissors = "Scissors";
+var paper = "Paper";
+var winner = "";
+var gameCount = 0;
 
 function playerChoice() {
     if(document.getElementById('rock').checked) {
@@ -16,9 +22,40 @@ function playerChoice() {
 function play() {
     playerChoice();
     var random = Math.floor(Math.random()*3);
-    var computerOutput = option[random];
-    document.getElementById("computerOutput").innerHTML = computerOutput;
+    computerResult = option[random];
+    document.getElementById("computerOutput").innerHTML = computerResult;
     document.getElementById("userSelected").innerHTML = player;
+    document.getElementById("computerSelected").innerHTML = computerResult;
+    compare(player, computerResult);
+    document.getElementById("result").innerHTML = "The winner is: " + winner;
+}
 
-
+function compare(player, computerResult) {
+    if (player === rock) {
+        if(computerResult === paper) {
+            winner = "Computer";
+        } else if (computerResult === rock) {
+            winner = "Tie";
+        } else {
+            winner = "Player";
+        }
+    }
+    if (player === paper) {
+        if(computerResult === scissors) {
+            winner = "Computer";
+        } else if (computerResult === paper) {
+            winner = "Tie";
+        } else {
+            winner = "Player";
+        }
+    }
+    if (player === scissors) {
+        if(computerResult === rock) {
+            winner = "Computer";
+        } else if (computerResult === scissors) {
+            winner = "Tie";
+        } else {
+            winner = "Player";
+        }
+    }
 }
