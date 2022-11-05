87e7c37efb3a3a1bd5066b71525cf39958adb3ff
Matthew <matthewvandyke08@gmail.com>
2022-11-05 15:08:04 -0700

    Computer History

--- game.js
+++ game.js
@@ -8,6 +8,9 @@ var scissors = "Scissors";
 var paper = "Paper";
 var winner = "";
 var gameCount = 0;
+var compRock = 0;
+var compPaper = 0;
+var compScissors = 0;
 
 function playerChoice() {
     if(document.getElementById('rock').checked) {
@@ -21,15 +24,24 @@ function playerChoice() {
 
 function play() {
     playerChoice();
+    // Decide Comp
     var random = Math.floor(Math.random()*3);
     computerResult = option[random];
+    // Update Game Count
     gameCount++;
     document.getElementById("historysection").innerHTML = gameCount;
+    // Display selections
     document.getElementById("computerOutput").innerHTML = computerResult;
     document.getElementById("userSelected").innerHTML = player;
     document.getElementById("computerSelected").innerHTML = computerResult;
+    // Get Winner
     compare(player, computerResult);
     document.getElementById("result").innerHTML = "The winner is: " + winner;
+    // Computer Choices
+    compChoiceCalc(computerResult);
+    document.getElementById("rocksection").innerHTML = compRock;
+    document.getElementById("papersection").innerHTML = compPaper;
+    document.getElementById("scissorssection").innerHTML = compScissors;
 }
 
 function compare(player, computerResult) {
@@ -59,5 +71,15 @@ function compare(player, computerResult) {
         } else {
             winner = "Player";
         }
+    }
+}
+
+function compChoiceCalc(computerResult) {
+    if(computerResult === "Rock") {
+        compRock++;
+    } else if(computerResult === "Paper"){
+        compPaper++;
+    } else {
+        compScissors++;
     }
 }
