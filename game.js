4bb54904a1cb09eac71400332cb6548433222d62
Matthew <matthewvandyke08@gmail.com>
2022-11-04 20:41:40 -0700

    Javascript track inputs and random selection

--- game.js
+++ game.js
@@ -1,2 +1,24 @@
-var name = window.prompt("What is your name?");
+var name = window.prompt("Please enter your name?");
 document.write(name + ", Welcome to Rock Paper Scissors!");
+var option = ["rock", "paper", "scissors"];
+var player = "";
+
+function playerChoice() {
+    if(document.getElementById('rock').checked) {
+        player = "Rock";
+    } else if(document.getElementById("paper").checked) {
+        player = "Paper";
+    } else {
+        player = "Scissors";
+    }
+}
+
+function play() {
+    playerChoice();
+    var random = Math.floor(Math.random()*3);
+    var computerOutput = option[random];
+    document.getElementById("computerOutput").innerHTML = computerOutput;
+    document.getElementById("userSelected").innerHTML = player;
+
+
+}
